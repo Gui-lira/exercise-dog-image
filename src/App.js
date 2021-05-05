58f414d794977307b39fdd8d003ec1983e5fcdb4
Gui-lira <gui.lira.martins@gmail.com>
2021-05-05 15:33:50 -0300

    1 e parte do 2

--- src/App.js
+++ src/App.js
@@ -2,29 +2,49 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.fecthDog = this.fecthDog.bind(this);
+    this.state = {
+      dogs: '',
+      loading: false
+    };
+  }
+
+  componentDidMount() {
+    this.fecthDog();
+  }
+
+  async fecthDog() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const fecthApi = await fetch('https://dog.ceo/api/breeds/image/random');
+        const apiJson = await fecthApi.json();
+        const src = await apiJson.message;
+        if (!src.includes('terrier')) {
+          this.setState({
+            dogs: src,
+            loading: false,
+          });
+        } else {
+          this.setState({ loading: false });
+        }
+      },
+    );
+  }
+
+  render() {
+    const { dogs, loading } = this.state;
+    return (
+      <div className="App">
+        <h1>Sempre um novo doguinho</h1>
+        {loading ? <h1>Loading</h1> : <img src={ dogs } alt="a dog" />}
+        <button type="button" onClick={ this.fecthDog }>Novo dog!</button>
+      </div>
+    );
+  }
 }
 
 export default App;
